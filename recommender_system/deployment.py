52ec53df4b6b229666818bed93d012b7301ffa58
ryantay93 <ryantay93@yahoo.com>
2023-11-17 09:47:32 +0800

    Still trying to add whitenoise 3

--- recommender_system/deployment.py
+++ recommender_system/deployment.py
@@ -18,8 +18,8 @@ MIDDLEWARE = [
 ]
 
 # Might need to pip install whitenoise for static files
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = BASE_DIR / 'static'
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
